51ad3766a8ed29acd02441a8271edaf4aea7f703
Steve Phillips <sjphilli@uwaterloo.ca>
2019-03-29 09:09:09 -0400

    Fix for adding catkin package in BEAM_MODULE DEPENDS

---
+++ cmake/ImportCatkin.cmake
@@ -0,0 +1,10 @@
+# Temporary fix to create imported target for catkin
+# Could be extended in the future to create targets for each package
+# we find in catkin.
+
+IF(catkin_FOUND AND NOT TARGET catkin::catkin)
+  ADD_LIBRARY(catkin::catkin INTERFACE IMPORTED)
+  SET_PROPERTY(TARGET catkin::catkin PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES "${catkin_INCLUDE_DIRS}")
+  target_link_libraries(catkin::catkin INTERFACE ${catkin_LIBRARIES})
+ENDIF()
