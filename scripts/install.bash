55f13d9694d16a30de663ab5113f849475cea497
Nick Charron <nicholas.c.charron@gmail.com>
2019-04-01 10:09:17 -0400

    update install to not install cmake

--- scripts/install.bash
+++ scripts/install.bash
@@ -61,7 +61,7 @@ install_routine()
     # Ensure wget is available
     sudo apt-get install -qq wget  > /dev/null
     # Install dependencies
-    install_cmake
+    #install_cmake
     install_catch2
     install_eigen3
     #install_ceres
