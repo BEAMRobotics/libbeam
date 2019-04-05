7f28b1b086553e86d4c7996b07d210bcff42fcd7
nickcharron <nicholas.c.charron@gmail.com>
2019-04-05 12:25:32 -0400

    removing config module - needs more work to avoid libwave conflict

--- beam_utils/include/beam/utils/utils.hpp
+++ beam_utils/include/beam/utils/utils.hpp
@@ -9,7 +9,6 @@
 #ifndef BEAM_UTILS_UTILS_HPP
 #define BEAM_UTILS_UTILS_HPP
 
-#include "beam/utils/config.hpp"
 #include "beam/utils/math.hpp"
 #include "beam/utils/time.hpp"
 #include "beam/utils/angles.hpp"
