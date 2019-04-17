a57642b57ece222465b37ddd72065cd0c808916e
Steve Phillips <sjphilli@uwaterloo.ca>
2019-04-17 17:41:01 -0400

    Fixing importing catkin::catkin target to include library

--- cmake/ImportCatkin.cmake
+++ cmake/ImportCatkin.cmake
@@ -1,10 +1,10 @@
 # Temporary fix to create imported target for catkin
 # Could be extended in the future to create targets for each package
 # we find in catkin.
-
 IF(catkin_FOUND AND NOT TARGET catkin::catkin)
   ADD_LIBRARY(catkin::catkin INTERFACE IMPORTED)
-  SET_PROPERTY(TARGET catkin::catkin PROPERTY
-    INTERFACE_INCLUDE_DIRECTORIES "${catkin_INCLUDE_DIRS}")
-    #target_link_libraries(catkin::catkin INTERFACE ${catkin_LIBRARIES})
+  SET_TARGET_PROPERTIES(catkin::catkin PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${catkin_INCLUDE_DIRS}"
+    INTERFACE_LINK_LIBRARIES "${catkin_LIBRARIES}")
 ENDIF()
+
