4ea21b6db6fd79ef2fcbe42691aaafac2affe9ab
emmclaugh <em3mclau@edu.uwaterloo.ca>
2019-08-26 12:49:42 -0400

    formatting

--- beam_colorize/src/RayTrace.cpp
+++ beam_colorize/src/RayTrace.cpp
@@ -65,40 +65,40 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr RayTrace::ColorizePointCloud() const {
     beam_cv::RayCastXYZRGB(image_, cloud_colored, hit_mask, hit_threshold_,
                            intrinsics_, HitBehaviour);
   }
-    return cloud_colored;
+  return cloud_colored;
 }
 
-  std::tuple<pcl::PointCloud<pcl::PointXYZRGB>::Ptr, std::vector<int>>
-      RayTrace::ReduceCloud(
-          pcl::PointCloud<pcl::PointXYZRGB>::Ptr input,
-          std::shared_ptr<cv::Mat> image,
-          std::shared_ptr<beam_calibration::CameraModel> intrinsics) const {
-    // cloud to search on
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(
-        new pcl::PointCloud<pcl::PointXYZRGB>);
-    std::vector<int> indices;
-    beam::Vec3 point;
-    for (uint32_t i = 0; i < input->points.size(); i++) {
-      point << input->points[i].x, input->points[i].y, input->points[i].z;
-      beam::Vec2 coords = intrinsics->ProjectPoint(point);
-      uint16_t u = std::round(coords(0, 0)), v = std::round(coords(1, 0));
-      uint16_t vmax = image->rows;
-      uint16_t umax = image->cols;
-      if (u > 0 && v > 0 && v < vmax && u < umax && point(2, 0) > 0) {
-        pcl::PointXYZRGB new_point;
-        new_point.x = point(0, 0);
-        new_point.y = point(1, 0);
-        new_point.z = point(2, 0);
-        cloud->points.push_back(new_point);
-        indices.push_back(i);
-      }
+std::tuple<pcl::PointCloud<pcl::PointXYZRGB>::Ptr, std::vector<int>>
+    RayTrace::ReduceCloud(
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr input,
+        std::shared_ptr<cv::Mat> image,
+        std::shared_ptr<beam_calibration::CameraModel> intrinsics) const {
+  // cloud to search on
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(
+      new pcl::PointCloud<pcl::PointXYZRGB>);
+  std::vector<int> indices;
+  beam::Vec3 point;
+  for (uint32_t i = 0; i < input->points.size(); i++) {
+    point << input->points[i].x, input->points[i].y, input->points[i].z;
+    beam::Vec2 coords = intrinsics->ProjectPoint(point);
+    uint16_t u = std::round(coords(0, 0)), v = std::round(coords(1, 0));
+    uint16_t vmax = image->rows;
+    uint16_t umax = image->cols;
+    if (u > 0 && v > 0 && v < vmax && u < umax && point(2, 0) > 0) {
+      pcl::PointXYZRGB new_point;
+      new_point.x = point(0, 0);
+      new_point.y = point(1, 0);
+      new_point.z = point(2, 0);
+      cloud->points.push_back(new_point);
+      indices.push_back(i);
     }
-    return std::make_tuple(cloud, indices);
   }
+  return std::make_tuple(cloud, indices);
+}
 
-  pcl::PointCloud<beam_containers::PointBridge>::Ptr RayTrace::ColorizeMask()
-      const {
-    return pcl::PointCloud<beam_containers::PointBridge>::Ptr();
-  }
+pcl::PointCloud<beam_containers::PointBridge>::Ptr
+    RayTrace::ColorizeMask() const {
+  return pcl::PointCloud<beam_containers::PointBridge>::Ptr();
+}
 
 } // namespace beam_colorize
