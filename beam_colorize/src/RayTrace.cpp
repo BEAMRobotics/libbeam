4d9af7043c593d831ff5233f8883909d82a1e0aa
emmclaugh <em3mclau@edu.uwaterloo.ca>
2019-08-26 12:42:56 -0400

    add check to remove NaNs

--- beam_colorize/src/RayTrace.cpp
+++ beam_colorize/src/RayTrace.cpp
@@ -17,8 +17,8 @@ void HitBehaviour(std::shared_ptr<cv::Mat> image,
 RayTrace::RayTrace() : Colorizer() {}
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr RayTrace::ColorizePointCloud() const {
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_colored(
-      new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_colored =
+      boost::make_shared<pcl::PointCloud<pcl::PointXYZRGB>>();
 
   pcl::copyPointCloud(*input_point_cloud_, *cloud_colored);
 
@@ -36,66 +36,69 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr RayTrace::ColorizePointCloud() const {
   auto indices = std::get<1>(reduced_cloud);
   // create kdtree for faster searching
   pcl::KdTreeFLANN<pcl::PointXYZRGB> kdtree;
-  kdtree.setInputCloud(input_cloud);
-  // create image mask where white pixels = projection hit
 
-  cv::Mat tmp = cv::Mat::zeros(image_->size(), CV_8UC3);
+  uint32_t points_colored = 0;
+  if (input_cloud->size() > 0) {
+    kdtree.setInputCloud(input_cloud);
+    // create image mask where white pixels = projection hit
 
-  cv::Mat hit_mask;
-  for (uint32_t i = 0; i < input_cloud->points.size(); i++) {
+    cv::Mat tmp = cv::Mat::zeros(image_->size(), CV_8UC3);
+
+    cv::Mat hit_mask;
+    for (uint32_t i = 0; i < input_cloud->points.size(); i++) {
+      beam::Vec3 point;
+      point << input_cloud->points[i].x, input_cloud->points[i].y,
+          input_cloud->points[i].z;
+      beam::Vec2 coords;
+
+      coords = intrinsics_->ProjectPoint(point);
+
+      uint16_t u = std::round(coords(0, 0)), v = std::round(coords(1, 0));
+      if (u > 0 && v > 0 && v < image_->rows && u < image_->cols) {
+        tmp.at<cv::Vec3b>(v, u).val[0] = 255;
+      }
+    }
+    cv::dilate(tmp, hit_mask, cv::Mat(dilation_, dilation_, CV_8UC1),
+               cv::Point(-1, -1), 1, 1, 1);
+
+    // This lambda performs ray tracing in parallel on each pixel in the image
+    beam_cv::RayCastXYZRGB(image_, cloud_colored, hit_mask, hit_threshold_,
+                           intrinsics_, HitBehaviour);
+  }
+    return cloud_colored;
+}
+
+  std::tuple<pcl::PointCloud<pcl::PointXYZRGB>::Ptr, std::vector<int>>
+      RayTrace::ReduceCloud(
+          pcl::PointCloud<pcl::PointXYZRGB>::Ptr input,
+          std::shared_ptr<cv::Mat> image,
+          std::shared_ptr<beam_calibration::CameraModel> intrinsics) const {
+    // cloud to search on
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(
+        new pcl::PointCloud<pcl::PointXYZRGB>);
+    std::vector<int> indices;
     beam::Vec3 point;
-    point << input_cloud->points[i].x, input_cloud->points[i].y,
-        input_cloud->points[i].z;
-    beam::Vec2 coords;
-
-    coords = intrinsics_->ProjectPoint(point);
-
-    uint16_t u = std::round(coords(0, 0)), v = std::round(coords(1, 0));
-    if (u > 0 && v > 0 && v < image_->rows && u < image_->cols) {
-      tmp.at<cv::Vec3b>(v, u).val[0] = 255;
+    for (uint32_t i = 0; i < input->points.size(); i++) {
+      point << input->points[i].x, input->points[i].y, input->points[i].z;
+      beam::Vec2 coords = intrinsics->ProjectPoint(point);
+      uint16_t u = std::round(coords(0, 0)), v = std::round(coords(1, 0));
+      uint16_t vmax = image->rows;
+      uint16_t umax = image->cols;
+      if (u > 0 && v > 0 && v < vmax && u < umax && point(2, 0) > 0) {
+        pcl::PointXYZRGB new_point;
+        new_point.x = point(0, 0);
+        new_point.y = point(1, 0);
+        new_point.z = point(2, 0);
+        cloud->points.push_back(new_point);
+        indices.push_back(i);
+      }
     }
+    return std::make_tuple(cloud, indices);
   }
-  cv::dilate(tmp, hit_mask, cv::Mat(dilation_, dilation_, CV_8UC1),
-             cv::Point(-1, -1), 1, 1, 1);
 
-  // This lambda performs ray tracing in parallel on each pixel in the image
-  beam_cv::RayCastXYZRGB(image_, cloud_colored, hit_mask, hit_threshold_,
-                         intrinsics_, HitBehaviour);
-
-  return cloud_colored;
-}
-
-std::tuple<pcl::PointCloud<pcl::PointXYZRGB>::Ptr, std::vector<int>>
-    RayTrace::ReduceCloud(
-        pcl::PointCloud<pcl::PointXYZRGB>::Ptr input,
-        std::shared_ptr<cv::Mat> image,
-        std::shared_ptr<beam_calibration::CameraModel> intrinsics) const {
-  // cloud to search on
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(
-      new pcl::PointCloud<pcl::PointXYZRGB>);
-  std::vector<int> indices;
-  beam::Vec3 point;
-  for (uint32_t i = 0; i < input->points.size(); i++) {
-    point << input->points[i].x, input->points[i].y, input->points[i].z;
-    beam::Vec2 coords = intrinsics->ProjectPoint(point);
-    uint16_t u = std::round(coords(0, 0)), v = std::round(coords(1, 0));
-    uint16_t vmax = image->rows;
-    uint16_t umax = image->cols;
-    if (u > 0 && v > 0 && v < vmax && u < umax && point(2, 0) > 0) {
-      pcl::PointXYZRGB new_point;
-      new_point.x = point(0, 0);
-      new_point.y = point(1, 0);
-      new_point.z = point(2, 0);
-      cloud->points.push_back(new_point);
-      indices.push_back(i);
-    }
+  pcl::PointCloud<beam_containers::PointBridge>::Ptr RayTrace::ColorizeMask()
+      const {
+    return pcl::PointCloud<beam_containers::PointBridge>::Ptr();
   }
-  return std::make_tuple(cloud, indices);
-}
-
-pcl::PointCloud<beam_containers::PointBridge>::Ptr
-    RayTrace::ColorizeMask() const {
-  return pcl::PointCloud<beam_containers::PointBridge>::Ptr();
-}
 
 } // namespace beam_colorize
