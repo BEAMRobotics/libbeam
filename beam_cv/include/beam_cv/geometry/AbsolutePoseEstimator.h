7d5abeab6b83ebee37f01a545121e868f58c7645
Jake McLaughlin <jake.mclaughlin98@gmail.com>
2020-10-26 15:18:14 -0400

    Fix function params

--- beam_cv/include/beam_cv/geometry/AbsolutePoseEstimator.h
+++ beam_cv/include/beam_cv/geometry/AbsolutePoseEstimator.h
@@ -18,34 +18,28 @@ namespace beam_cv {
 class AbsolutePoseEstimator {
 public:
   /**
-   * @brief Computes the essential matrix for 2 cameras given associated pixels
-   * @param camR camera model for image 1
-   * @param camC camera model for image 2
-   * @param pixels_r projected pixel locations of points in camR (min size = 3)
-   * @param pixels_c projected pixel locations of points in camC (min size = 3)
+   * @brief Computes the absolute pose of camera
+   * @param cam camera model for image 
+   * @param pixels projected pixel locations of points in cam (min size = 3)
    * @param points 3d feature locations (min size = 3)
    */
   static opt<Eigen::Matrix4d>
-      P3PEstimator(std::shared_ptr<beam_calibration::CameraModel> camR,
-                   std::shared_ptr<beam_calibration::CameraModel> camC,
-                   std::vector<Eigen::Vector2i> pixels_r,
-                   std::vector<Eigen::Vector2i> pixels_c,
+      P3PEstimator(std::shared_ptr<beam_calibration::CameraModel> cam,
+                   std::vector<Eigen::Vector2i> pixels,
                    std::vector<Eigen::Vector3d> points);
 
   /**
    * @brief Computes the essential matrix for 2 cameras given associated pixels
-   * @param camR camera model for image 1
-   * @param camC camera model for image 2
-   * @param pixels_r projected pixel locations of points in camR
-   * @param pixels_c projected pixel locations of points in camC
-   * @param points 3d feature locations
+   * @param cam camera model for image 
+   * @param pixels projected pixel locations of points in cam
+   * @param points 3d feature locations (min size = 3)
+   * @param max_iterations # of iteratiosn to perform
+   * @param inlier_threshold pixel distance to accept as inlier
    */
   static opt<Eigen::Matrix4d>
-      RANSACP3PEstimator(std::shared_ptr<beam_calibration::CameraModel> camR,
-                         std::shared_ptr<beam_calibration::CameraModel> camC,
-                         std::vector<Eigen::Vector2i> pixels_r,
-                         std::vector<Eigen::Vector2i> pixels_c,
-                         std::vector<Eigen::Vector3d> points,
+      RANSACP3PEstimator(std::shared_ptr<beam_calibration::CameraModel> cam,
+                   std::vector<Eigen::Vector2i> pixels,
+                   std::vector<Eigen::Vector3d> points,
                          int max_iterations, double inlier_threshold);
 };
 
