2c020a2df15006616a2c27d1ddbad3e52c97c6d9
Jake McLaughlin <jake.mclaughlin98@gmail.com>
2020-10-28 17:38:59 -0400

    change function signature

--- beam_cv/include/beam_cv/geometry/AbsolutePoseEstimator.h
+++ beam_cv/include/beam_cv/geometry/AbsolutePoseEstimator.h
@@ -22,25 +22,12 @@ public:
    * @param cam camera model for image 
    * @param pixels projected pixel locations of points in cam (min size = 3)
    * @param points 3d feature locations (min size = 3)
+   * @returns 4 possible transformation matrices
    */
-  static opt<Eigen::Matrix4d>
+  static std::vector<Eigen::Matrix4d>
       P3PEstimator(std::shared_ptr<beam_calibration::CameraModel> cam,
                    std::vector<Eigen::Vector2i> pixels,
                    std::vector<Eigen::Vector3d> points);
-
-  /**
-   * @brief Computes the essential matrix for 2 cameras given associated pixels
-   * @param cam camera model for image 
-   * @param pixels projected pixel locations of points in cam
-   * @param points 3d feature locations (min size = 3)
-   * @param max_iterations # of iteratiosn to perform
-   * @param inlier_threshold pixel distance to accept as inlier
-   */
-  static opt<Eigen::Matrix4d>
-      RANSACP3PEstimator(std::shared_ptr<beam_calibration::CameraModel> cam,
-                   std::vector<Eigen::Vector2i> pixels,
-                   std::vector<Eigen::Vector3d> points,
-                         int max_iterations, double inlier_threshold);
 };
 
 } // namespace beam_cv
