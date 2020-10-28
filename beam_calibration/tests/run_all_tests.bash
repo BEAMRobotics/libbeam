fbadd014fb750cd61fd14ab6bd8be5764b8dceeb
Jake McLaughlin <jake.mclaughlin98@gmail.com>
2020-10-28 18:48:44 -0400

    Add test to script

--- beam_calibration/tests/run_all_tests.bash
+++ beam_calibration/tests/run_all_tests.bash
@@ -6,3 +6,5 @@ cd ./beam_calibration/
 ./beam_calibration_double_sphere_tests
 ./beam_calibration_kannala_brandt_tests
 ./beam_calibration_radtan_tests
+./beam_calibration_ladybug_tests
+./beam_calibration_extrinsics
