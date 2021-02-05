1909cfbceb089b450675cddaad653c38a365a0e8
nickcharron <nicholas.c.charron@gmail.com>
2021-02-05 16:52:59 -0700

    add matchers header

---
+++ beam_matching/include/beam_matching/Matchers.h
@@ -0,0 +1,14 @@
+/** @file
+ * @ingroup matching
+ *
+ * @defgroup matching
+ * Classes to perform scan matching/registration.
+ * There are a variety of different scan matching algorithms
+ * available with a similar interface.
+ */
+
+#pragma once
+
+#include <beam_matching/IcpMatcher.h>
+#include <beam_matching/GicpMatcher.h>
+#include <beam_matching/NdtMatcher.h>
