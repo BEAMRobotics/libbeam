787c4dc9757b7d5a2c5b62a67d20431c37e49127
Jake McLaughlin <jake.mclaughlin98@gmail.com>
2021-06-01 16:16:54 -0400

    remove unused function

--- beam_cv/tests/feature_tests.cpp
+++ beam_cv/tests/feature_tests.cpp
@@ -20,29 +20,6 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
-std::string type2str(int type) {
-  std::string r;
-
-  uchar depth = type & CV_MAT_DEPTH_MASK;
-  uchar chans = 1 + (type >> CV_CN_SHIFT);
-
-  switch (depth) {
-    case CV_8U: r = "8U"; break;
-    case CV_8S: r = "8S"; break;
-    case CV_16U: r = "16U"; break;
-    case CV_16S: r = "16S"; break;
-    case CV_32S: r = "32S"; break;
-    case CV_32F: r = "32F"; break;
-    case CV_64F: r = "64F"; break;
-    default: r = "User"; break;
-  }
-
-  r += "C";
-  r += (chans + '0');
-
-  return r;
-}
-
 std::shared_ptr<beam_cv::Matcher> matcher =
     std::make_shared<beam_cv::FLANNMatcher>(beam_cv::FLANN::KDTree, 0.8, true,
                                             true, cv::FM_RANSAC, 5);
