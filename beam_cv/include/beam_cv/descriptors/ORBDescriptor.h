f05173e0be22d6432eb38b3fde109bbb4007225e
nickcharron <nicholas.c.charron@gmail.com>
2021-06-22 18:27:30 -0400

    refactor descriptors

--- beam_cv/include/beam_cv/descriptors/ORBDescriptor.h
+++ beam_cv/include/beam_cv/descriptors/ORBDescriptor.h
@@ -17,6 +17,35 @@ namespace beam_cv {
  */
 class ORBDescriptor : public Descriptor {
 public:
+  struct Params {
+    /** The number of points that compose the ORB descriptor. A
+     * value of 2 constructs the descriptor by comparing the brightnesses of a
+     * random pair of points. Other values are 3 or 4; 3 will use 3 random
+     * points, and 4 similarly will use 4 random points. For these last two
+     * options, the output result will be 2 bits, as the result is the index of
+     * the brightest point (0, 1, 2, 3). Thus, the distance method used for the
+     *  BruteForceMatcher or FLANNMatcher will have to be cv::NORM_HAMMING_2, a
+     *  variation to the Hamming distance, when tuple_size = 3 or 4. Options: 2,
+     * 3, or 4.
+     */
+    int tuple_size = 2;
+
+    /** The size of the square patch used in the random point
+     * sampling to construct the descriptor. This patch is smoothed using an
+     * integral image. Default: 31 (31 x 31 pixels). Must be greater than 0.
+     */
+    int patch_size = 31;
+
+    // load params from json. If empty, it will use default params
+    void LoadFromJson(const std::string& config_path);
+  };
+
+  /**
+   * @brief Constructor that requires a params object
+   * @param params see struct above
+   */
+  ORBDescriptor(const Params& params);
+
   /**
    * @brief Constructor
    * @param tuple_size The number of points that compose the ORB descriptor. A
@@ -49,8 +78,11 @@ public:
                              std::vector<cv::KeyPoint>& keypoints);
 
 private:
-  int tuple_size_;
-  int patch_size_;
+  // this gets called in each constructor
+  void Setup();
+
+  Params params_;
+
   /** The pointer to the wrapped cv::ORB object. */
   cv::Ptr<cv::ORB> orb_descriptor_;
 
--- beam_cv/src/descriptors/BRISKDescriptor.cpp
+++ beam_cv/src/descriptors/BRISKDescriptor.cpp
@@ -1,17 +1,51 @@
 #include <beam_cv/descriptors/BRISKDescriptor.h>
 
+#include <boost/filesystem.hpp>
+#include <nlohmann/json.hpp>
+
 namespace beam_cv {
+
+void BRISKDescriptor::Params::LoadFromJson(const std::string& config_path) {
+  if (config_path.empty()) { return; }
+
+  if (!boost::filesystem::exists(config_path)) {
+    BEAM_ERROR("Invalid file path for SIFT descriptor params, using default "
+               "params. Input: {}",
+               config_path);
+    return;
+  }
+
+  nlohmann::json J;
+  std::ifstream file(config_path);
+  file >> J;
+
+  rlist.clear();
+  for (const auto& value : J["rlist"]) { rlist.push_back(value.get<float>()); }
+
+  nlist.clear();
+  for (const auto& value : J["nlist"]) { nlist.push_back(value.get<int>()); }
+
+  d_max = J["d_max"];
+  d_min = J["d_min"];
+}
+
+BRISKDescriptor::BRISKDescriptor(const Params& params) : params_(params){Setup();};
 
 // Default constructor. Struct may be default or user defined.
 BRISKDescriptor::BRISKDescriptor(const std::vector<float>& rlist,
                                  const std::vector<int>& nlist, float d_max,
                                  float d_min) {
-  this->radius_list_ = rlist;
-  this->number_list_ = nlist;
-  this->d_max_ = d_max;
-  this->d_min_ = d_min;
+  params_.rlist = rlist;
+  params_.nlist = nlist;
+  params_.d_max = d_max;
+  params_.d_min = d_min;
+  Setup();
+}
+
+void BRISKDescriptor::Setup(){
   // Ensure parameters are valid
-  this->CheckConfig();
+  CheckConfig();
+
   // OpenCV refers to this as a parameter for "index remapping of the bits."
   // Kaehler and Bradski's book, "Learning OpenCV3: Computer Vision in C++
   // with the OpenCV Library" states this parameter is unused, and should be
@@ -19,39 +53,40 @@ BRISKDescriptor::BRISKDescriptor(const std::vector<float>& rlist,
   std::vector<int> index_change;
 
   // Create cv::BRISK object with the desired parameters
-  this->brisk_descriptor_ =
-      cv::BRISK::create(this->radius_list_, this->number_list_, this->d_max_,
-                        this->d_min_, index_change);
+  brisk_descriptor_ = cv::BRISK::create(
+      params_.rlist, params_.nlist, params_.d_max, params_.d_min, index_change);
 }
 
 void BRISKDescriptor::CheckConfig() {
   // Check that the size of radiusList and numberList are equal and positive
-  if (this->radius_list_.size() == 0) {
-    throw std::invalid_argument("No parameters in radius_list!");
-  } else if (this->number_list_.size() == 0) {
-    throw std::invalid_argument("No parameters in number_list!");
-  } else if (this->radius_list_.size() != this->number_list_.size()) {
-    throw std::invalid_argument(
-        "radius_list and number_list are of unequal size!");
+  if (params_.rlist.size() == 0) {
+    throw std::invalid_argument("No parameters in rlist!");
+  } else if (params_.nlist.size() == 0) {
+    throw std::invalid_argument("No parameters in nlist!");
+  } else if (params_.rlist.size() != params_.nlist.size()) {
+    throw std::invalid_argument("rlist and nlist are of unequal size!");
   }
+
   // Ensure all values of radiusList are positive
-  for (const auto& radius : this->radius_list_) {
+  for (const auto& radius : params_.rlist) {
     if (radius < 0) {
       throw std::invalid_argument("radius_list has a negative parameter!");
     }
   }
+
   // Ensure all values of numberList are positive
-  for (auto& num_points : this->number_list_) {
+  for (auto& num_points : params_.nlist) {
     if (num_points < 0) {
-      throw std::invalid_argument("number_list has a negative parameter!");
+      throw std::invalid_argument("nlist has a negative parameter!");
     }
   }
+
   // Ensure dMax and dMin are both positive, and check dMax is less than dMin
-  if (this->d_max_ < 0) {
+  if (params_.d_max < 0) {
     throw std::invalid_argument("d_max is a negative value!");
-  } else if (this->d_min_ < 0) {
+  } else if (params_.d_min < 0) {
     throw std::invalid_argument("d_min is a negative value!");
-  } else if (this->d_max_ > this->d_min_) {
+  } else if (params_.d_max > params_.d_min) {
     throw std::invalid_argument("d_max is greater than d_min!");
   }
 }
@@ -60,7 +95,7 @@ cv::Mat
     BRISKDescriptor::ExtractDescriptors(const cv::Mat& image,
                                         std::vector<cv::KeyPoint>& keypoints) {
   cv::Mat descriptors;
-  this->brisk_descriptor_->compute(image, keypoints, descriptors);
+  brisk_descriptor_->compute(image, keypoints, descriptors);
   return descriptors;
 }
 } // namespace beam_cv
