ac063b8252255406c5204ba8f6bb8784e0b7ff92
Gabe <earlegabriel1@gmail.com>
2021-08-03 08:15:34 -0400

    merge bug

--- beam_cv/src/detectors/SIFTDetector.cpp
+++ beam_cv/src/detectors/SIFTDetector.cpp
@@ -55,7 +55,7 @@ void SIFTDetector::Setup() {
   int num_features_per_grid =
       params_.num_features / (params_.grid_cols * params_.grid_rows);
 
-  sift_detector_ = cv::xfeatures2d::SIFT::create(
+  sift_detector_ = cv::SIFT::create(
       num_features_per_grid, params_.n_octave_layers,
       params_.contrast_threshold, params_.edge_threshold, params_.sigma);
 }
