25b6faa53fe1df7850e05a682b9d43015cc75192
nickcharron <nicholas.c.charron@gmail.com>
2021-08-06 16:16:23 -0400

    change key to string

--- beam_utils/src/filesystem.cpp
+++ beam_utils/src/filesystem.cpp
@@ -75,7 +75,7 @@ nlohmann::json ToJsonPoseObject(uint64_t t, const Eigen::Matrix4d& T) {
 }
 
 void AddPoseToJson(nlohmann::json& J, uint64_t t, const Eigen::Matrix4d& T) {
-  J[t] = {T(0, 0), T(0, 1), T(0, 2), T(0, 3), T(1, 0), T(1, 1),
+  J[std::to_string(t)] = {T(0, 0), T(0, 1), T(0, 2), T(0, 3), T(1, 0), T(1, 1),
           T(1, 2), T(1, 3), T(2, 0), T(2, 1), T(2, 2), T(2, 3),
           T(3, 0), T(3, 1), T(3, 2), T(3, 3)};
 }
