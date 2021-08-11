5971b68b016a467310d927d0f713f26648a834c5
nickcharron <nicholas.c.charron@gmail.com>
2021-08-10 20:12:25 -0400

    fix opencv likage issues

--- beam_utils/include/beam_utils/math.h
+++ beam_utils/include/beam_utils/math.h
@@ -18,8 +18,6 @@
 
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
-#include <opencv2/core/core.hpp>
-#include <opencv2/opencv.hpp>
 #include <pcl/point_types.h>
 #include <unsupported/Eigen/MatrixFunctions>
 
