f1489dc993701bdedb6935935a380a8faf7a05ab
nickcharron <nicholas.c.charron@gmail.com>
2021-10-22 15:48:16 -0400

    clean up test

--- beam_optimization/tests/src/pose_prior_tests.cpp
+++ beam_optimization/tests/src/pose_prior_tests.cpp
@@ -2,89 +2,74 @@
 
 #include <Eigen/Geometry>
 #include <catch2/catch.hpp>
-#include <ceres/ceres.h>
-#include <ceres/loss_function.h>
-#include <ceres/solver.h>
-#include <ceres/types.h>
 
+#include <beam_utils/filesystem.h>
 #include <beam_utils/math.h>
 
+#include <beam_optimization/CeresParams.h>
 #include <beam_optimization/PosePriorCost.h>
 
+using namespace beam_optimization;
+
+std::string GetFilepathConfig(std::string filename) {
+  return beam::LibbeamRoot() + "beam_optimization/config/" + filename;
+}
+
 TEST_CASE("Test prior pose cost function with identity covariance") {
-  ceres::Problem::Options ceres_problem_options;
-  ceres_problem_options.loss_function_ownership = ceres::DO_NOT_TAKE_OWNERSHIP;
-  ceres_problem_options.local_parameterization_ownership =
-      ceres::DO_NOT_TAKE_OWNERSHIP;
-
-  std::unique_ptr<ceres::LocalParameterization> quat_parameterization(
-      new ceres::QuaternionParameterization());
-  std::unique_ptr<ceres::LocalParameterization> identity_parameterization(
-      new ceres::IdentityParameterization(3));
-  std::unique_ptr<ceres::LocalParameterization> parameterization_ =
-      std::unique_ptr<ceres::LocalParameterization>(
-          new ceres::ProductParameterization(
-              quat_parameterization.release(),
-              identity_parameterization.release()));
-  std::unique_ptr<ceres::LossFunction> loss_function_ =
-      std::unique_ptr<ceres::LossFunction>(new ceres::TrivialLoss());
-
-  ceres::Solver::Options ceres_solver_options;
-  ceres_solver_options.minimizer_progress_to_stdout = true;
-  ceres_solver_options.max_num_iterations = 100;
-  ceres_solver_options.max_solver_time_in_seconds = 1e6;
-  ceres_solver_options.function_tolerance = 1e-8;
-  ceres_solver_options.gradient_tolerance = 1e-10;
-  ceres_solver_options.parameter_tolerance = 1e-8;
-  ceres_solver_options.linear_solver_type = ceres::SPARSE_NORMAL_CHOLESKY;
+  std::string ceres_config = GetFilepathConfig("CeresParamsDefault.json");
+  CeresParams ceres_params(ceres_config);
 
   // initial estimate
-  Eigen::Matrix4d T_P = Eigen::Matrix4d::Identity();
-  T_P(2, 3) = 5;
-  Eigen::Quaterniond tp_q;
-  Eigen::Vector3d tp_p;
-  beam::TransformMatrixToQuaternionAndTranslation(T_P, tp_q, tp_p);
+  Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
+  T(2, 3) = 5;
+  Eigen::Quaterniond q;
+  Eigen::Vector3d t;
+  beam::TransformMatrixToQuaternionAndTranslation(T, q, t);
+
   // create perturbed initial
-  Eigen::VectorXd perturbation(6, 1);
-  perturbation << 10, -10, 10, 1, -1, 1;
-  Eigen::Matrix4d T_P_pert = beam::PerturbTransformDegM(T_P, perturbation);
-  Eigen::Quaterniond tpert_q;
-  Eigen::Vector3d tpert_p;
-  beam::TransformMatrixToQuaternionAndTranslation(T_P_pert, tpert_q, tpert_p);
+  Eigen::VectorXd pert(6, 1);
+  pert << 10, -10, 10, 0.1, -0.1, 0.1;
+  Eigen::Matrix4d T_pert = beam::PerturbTransformDegM(T, pert);
+  Eigen::Quaterniond q_pert;
+  Eigen::Vector3d t_pert;
+  beam::TransformMatrixToQuaternionAndTranslation(T_pert, q_pert, t_pert);
 
   // result
-  std::vector<double> results{tp_q.w(), tp_q.x(), tp_q.y(), tp_q.z(),
-                              tp_p[0],  tp_p[1],  tp_p[2]};
+  std::vector<double> results{q_pert.w(), q_pert.x(), q_pert.y(), q_pert.z(),
+                              t_pert[0],  t_pert[1],  t_pert[2]};
 
-  // identity covariance (absolute certainty)
-  Eigen::Matrix<double, 6, 6> covariance = Eigen::MatrixXd::Identity(6, 6);
+  // set low covariance
+  Eigen::Matrix<double, 6, 6> covariance =
+      Eigen::MatrixXd::Identity(6, 6) * 1e-8;
 
   // setup ceres problem
   std::shared_ptr<ceres::Problem> problem =
-      std::make_shared<ceres::Problem>(ceres_problem_options);
+      std::make_shared<ceres::Problem>(ceres_params.ProblemOptions());
 
-  problem->AddParameterBlock(&(results[0]), 7, parameterization_.get());
+  problem->AddParameterBlock(
+      &(results[0]), 7, ceres_params.SE3QuatTransLocalParametrization().get());
+
   std::unique_ptr<ceres::CostFunction> cost_function(
-      CeresPosePriorCostFunction::Create(T_P_pert, covariance));
-  problem->AddResidualBlock(cost_function.release(), loss_function_.get(),
-                            &(results[0]));
+      CeresPosePriorCostFunction::Create(T, covariance));
+
+  problem->AddResidualBlock(cost_function.release(),
+                            ceres_params.LossFunction().get(), &(results[0]));
 
   // solve problem
   ceres::Solver::Summary ceres_summary;
-  ceres::Solve(ceres_solver_options, problem.get(), &ceres_summary);
+  ceres::Solve(ceres_params.SolverOptions(), problem.get(), &ceres_summary);
   std::string report = ceres_summary.FullReport();
+  std::cout << "Ceres Report: \n" << report << "\n\n";
 
   // get pose from result
-  std::cout << "INITIAL" << std::endl;
-  std::cout << tp_q.w() << "," << tp_q.x() << "," << tp_q.y() << "," << tp_q.z()
-            << "," << tp_p[0] << "," << tp_p[1] << "," << tp_p[2] << std::endl;
-  std::cout << "RESULT" << std::endl;
+  std::cout << "INITIAL: [" << q.w() << ", " << q.x() << ", " << q.y() << ", "
+            << q.z() << ", " << t[0] << ", " << t[1] << ", " << t[2] << "]\n";
+  std::cout << "RESULT: [";
   for (auto& i : results) { std::cout << i << ", "; }
-  std::cout << std::endl;
-  std::cout << "EXPECTED" << std::endl;
-  std::cout << tpert_q.w() << "," << tpert_q.x() << "," << tpert_q.y() << ","
-            << tpert_q.z() << "," << tpert_p[0] << "," << tpert_p[1] << ","
-            << tpert_p[2] << std::endl;
+  std::cout << "]\n";
+  std::cout << "EXPECTED: [" << q_pert.w() << ", " << q_pert.x() << ", "
+            << q_pert.y() << "," << q_pert.z() << ", " << t_pert[0] << ", "
+            << t_pert[1] << ", " << t_pert[2] << "]\n";
 
-  // REQUIRE(beam::RoundMatrix(T_P_pert, 5) == beam::RoundMatrix(pose, 5));
+  // REQUIRE(beam::RoundMatrix(T_pert, 5) == beam::RoundMatrix(T, 5));
 }
