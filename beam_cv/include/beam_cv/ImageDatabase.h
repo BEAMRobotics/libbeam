ad3b03a41266b301e3a62b8aa48c24fbeee5e244
Jake McLaughlin <jake.mclaughlin98@gmail.com>
2022-09-07 18:17:02 -0400

    Initial implementation

---
+++ beam_cv/include/beam_cv/ImageDatabase.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <DBoW3/DBoW3.h>
+#include <nlohmann/json.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <beam_cv/descriptors/ORBDescriptor.h>
+#include <beam_cv/detectors/GFTTDetector.h>
+
+namespace json = nlohmann::json;
+
+const std::string DEFAULT_VOCAB_PATH = "";
+
+namespace beam_cv {
+
+/**
+ * @brief class for image database
+ */
+class ImageDatabase {
+public:
+  /**
+   * @brief Constructor to initialize with empty database
+   * @param database_path path to database folder
+   */
+  ImageDatabase(const GFTTDetector::Params& detector_params,
+                const ORBDescriptor::Params& descriptor_params);
+
+  /**
+   * @brief Constructor to initialize with already created dbow db
+   * @param database_path path to database folder
+   */
+  ImageDatabase(const GFTTDetector::Params& detector_params,
+                const ORBDescriptor::Params& descriptor_params,
+                const std::string& dbow_file_path,
+                const std::string& timestamps_file_path);
+
+  /**
+   * @brief Default destructor
+   */
+  ~ImageDatabase() = default;
+
+  /**
+   * @brief Save current database to default folder
+   */
+  void SaveDatabase(const std::string& dbow_file_path,
+                    const std::string& timestamps_file_path);
+
+  /**
+   * @brief Gets the word for a given descriptor
+   */
+  cv::Mat GetWord(const cv::Mat& descriptor);
+
+  /**
+   * @brief Return list of N image id's best matching query image
+   * @param query_image to query database with
+   */
+  std::vector<uint64_t> QueryDatabase(const cv::Mat& query_image, int N = 2);
+
+  /**
+   * @brief Add an image to the database
+   */
+  uint64_t AddImage(const cv::Mat& image, const ros::Time& timestamp);
+
+  /**
+   * @brief Gets the timestamp associated to image with index in the database
+   */
+  ros::Time GetImageTimestamp(uint64_t index);
+
+private:
+  json index_to_timestamp_map_;
+  std::shared_ptr<DBoW3::Database> dbow_db_;
+  beam_cv::ORBDescriptor descriptor_;
+  beam_cv::GFTTDetector detector_;
+};
+
+} // namespace beam_cv
---
+++ beam_cv/src/ImageDatabase.cpp
@@ -0,0 +1,76 @@
+#include <beam_cv/ImageDatabase.h>
+#include <beam_utils/optional.h>
+
+namespace beam_cv {
+
+ImageDatabase::ImageDatabase(const GFTTDetector::Params& detector_params,
+                             const ORBDescriptor::Params& descriptor_params, ) {
+  // construct detector and descriptor
+  detector_ = beam_cv::GFTTDetector(detector_params);
+  descriptor_ = beam_cv::ORBDescriptor(descriptor_params);
+  // construct database
+  DBoW3::Vocabulary default_vocab(DEFAULT_VOCAB_PATH);
+  dbow_db_ = std::make_shared<DBoW3::Database>(default_vocab);
+  index_to_timestamp_map_ = json::object();
+}
+
+ImageDatabase::ImageDatabase(const GFTTDetector::Params& detector_params,
+                             const ORBDescriptor::Params& descriptor_params,
+                             const std::string& dbow_file_path,
+                             const std::string& timestamps_file_path) {
+  // construct detector and descriptor
+  detector_ = beam_cv::GFTTDetector(detector_params);
+  descriptor_ = beam_cv::ORBDescriptor(descriptor_params);
+  // construct database
+  dbow_db_ = std::make_shared<DBoW3::Database>(dbow_file_path);
+  std::ifstream timestamps_file(timestamps_file_path);
+  index_to_timestamp_map_ = json::object();
+  timestamps_file >> index_to_timestamp_map_;
+}
+
+void ImageDatabase::SaveDatabase(const std::string& dbow_file_path,
+                                 const std::string& timestamps_file_path) {
+  dbow_db_->save(dbow_file_path);
+  std::ofstream timestamps_file(imestamps_file_path);
+  timestamps_file << std::setw(4) << index_to_timestamp_map_ << std::endl;
+}
+
+cv::Mat ImageDatabase::GetWord(const cv::Mat& descriptor) {
+  auto vocab = bow_db_->getVocabulary();
+  auto word_id = vocab.transform(descriptor);
+  return vocab.getWord(word_id);
+}
+
+std::vector<uint64_t> ImageDatabase::QueryDatabase(const cv::Mat& query_image,
+                                                   int N = 2) {
+  std::vector<cv::KeyPoint> kps = detector_.DetectFeatures(query_image);
+  cv::Mat features = descriptor_.ExtractDescriptors(query_image, kps);
+  DBoW3::QueryResults results;
+  bow_db_->query(features, results, N);
+
+  std::vector<uint> indices;
+  for (unsigned int i = 0; i < results.size(); i++) {
+    DBoW3::Result res = results[i];
+    indices.push_back(res.Id);
+  }
+  return indices;
+}
+
+uint64_t ImageDatabase::AddImage(const cv::Mat& image,
+                                 const ros::Time& timestamp) {
+  std::vector<cv::KeyPoint> kps = detector_.DetectFeatures(image);
+  cv::Mat features = descriptor_.ExtractDescriptors(image, kps);
+  unsigned int idx = bow_db_->add(features);
+  index_to_timestamp_map_[idx] = timestamp.toSec();
+  return idx;
+}
+
+beam::opt<ros::Time> ImageDatabase::GetImageTimestamp(uint64_t index) {
+  if (index_to_timestamp_map_.contains(index)) {
+    double time = index_to_timestamp_map_[index];
+    return ros::Time(time);
+  }
+  return {};
+}
+
+} // namespace beam_cv
