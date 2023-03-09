5cfaf1216018e3d0b9a2b3fcc283da120608e7e3
nickcharron <nicholas.c.charron@gmail.com>
2023-03-08 21:01:52 -0800

    add basic files

---
+++ beam_utils/include/beam_utils/bspline_utils.h
@@ -0,0 +1,17 @@
+/** @file
+ * @ingroup utils
+ *
+ * bspline fitting from: https://docs.openvins.com/
+ *
+ */
+
+#pragma once
+
+namespace beam {
+/** @addtogroup utils
+ *  @{ */
+
+
+
+/** @} group utils */
+} // namespace beam
---
+++ beam_utils/src/bspline.cpp
@@ -0,0 +1,8 @@
+#include <beam_utils/bspline.h>
+#include <beam_utils/bspline_utils.h>
+
+namespace beam {
+
+
+
+} // namespace beam
