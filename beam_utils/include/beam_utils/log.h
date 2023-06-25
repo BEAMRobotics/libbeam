11808c8c76b35eaaf13da8ae25cd53ab8e368b30
nickcharron <nicholas.c.charron@gmail.com>
2023-06-24 18:11:35 -0700

    upgrade to c++17 and add logging function

--- beam_utils/include/beam_utils/log.h
+++ beam_utils/include/beam_utils/log.h
@@ -13,10 +13,13 @@
 
 #pragma once
 
+#include <experimental/source_location>
 #include <iostream>
 
+#include <boost/filesystem.hpp>
+
 #ifndef NDEBUG
-  #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
+#  define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
 #endif
 #include "spdlog/spdlog.h"
 
@@ -24,14 +27,27 @@ namespace beam {
 /** @addtogroup utils
  *  @{ */
 
-#define FILENAME \
-    (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#define FILENAME                                                               \
+  (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
-#define LOG_ERROR(M, ...) \
-    fprintf(              \
-      stderr, "[ERROR] [%s:%d] " M "\n", FILENAME, __LINE__, ##__VA_ARGS__)
+#define LOG_ERROR(M, ...)                                                      \
+  fprintf(stderr, "[ERROR] [%s:%d] " M "\n", FILENAME, __LINE__, ##__VA_ARGS__)
 
 #define LOG_INFO(M, ...) fprintf(stdout, "[INFO] " M "\n", ##__VA_ARGS__)
+
+// Helper function for logging the source location of code.
+// This will cout: [FL: {FILE}][FN: ${Function}][L: {LINE}][post_fix]
+// call using: LogSourceLocation(beam::source_location::current(), "some text to
+// add")
+using source_location = std::experimental::source_location;
+inline void LogSourceLocation(const source_location& source_location,
+                              const std::string& post_fix = "") {
+  std::string filename =
+      boost::filesystem::path(source_location.file_name()).filename().string();
+  std::cout << "[F: " << filename
+            << "][FNC: " << source_location.function_name()
+            << "][L: " << source_location.line() << "][" << post_fix << "]\n";
+}
 
 inline void OutputPercentComplete(int current_, int total_,
                                   std::string message_) {
@@ -55,14 +71,11 @@ inline void OutputPercentComplete(int current_, int total_,
 }
 
 template <typename T>
-void PrintVector(const std::vector<T>& v){
+void PrintVector(const std::vector<T>& v) {
   std::cout << "[";
-  for (int i = 0; i < v.size() - 1; i++){
-    std::cout << v[i] << ", ";
-  }
+  for (int i = 0; i < v.size() - 1; i++) { std::cout << v[i] << ", "; }
   std::cout << v[v.size() - 1] << "]\n";
 }
 
 /** @} group utils */
-}  // namespace beam
-
+} // namespace beam
