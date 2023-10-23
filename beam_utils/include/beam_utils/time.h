8adcae1ddc0f0dceff428fbdbb1832c460d12498
Jake McLaughlin <jake.mclaughlin98@gmail.com>
2023-10-23 14:47:52 -0400

    Add scan context to beam matching

--- beam_utils/include/beam_utils/time.h
+++ beam_utils/include/beam_utils/time.h
@@ -179,6 +179,44 @@ private:
   std::uint64_t start_time;
 };
 
+class TicToc
+{
+public:
+    TicToc()
+    {
+        tic();
+    }
+
+    TicToc( bool _disp )
+    {
+        disp_ = _disp;
+        tic();
+    }
+
+    void tic()
+    {
+        start = std::chrono::system_clock::now();
+    }
+
+    void toc( std::string _about_task )
+    {
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        double elapsed_ms = elapsed_seconds.count() * 1000;
+
+        if( disp_ )
+        {
+          std::cout.precision(3); // 10 for sec, 3 for ms 
+          std::cout << _about_task << ": " << elapsed_ms << " msec." << std::endl;
+        }
+    }
+
+private:  
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    bool disp_ = false;
+};
+
+
 /** Similar to how matlab's tic and toc work, `tic()` starts the timer where the
  * variable `t` stores the start time, when `toc()` is called it returns the
  * time
